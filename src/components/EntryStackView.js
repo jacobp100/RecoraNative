f38b33794eb133b339bae517986c033093482293
Jacob Parker <jacobparker1992@gmail.com>
2016-10-14 10:11:17 +0100

    Fuck

--- src/components/EntryStackView.js
+++ src/components/EntryStackView.js
@@ -2,9 +2,17 @@
 import React, { Component } from 'react';
 import { View, Text, TextInput, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
-import { map, keys, get, getOr, last, matchesProperty, concat } from 'lodash/fp';
+import { map, keys, get, getOr, last, concat } from 'lodash/fp';
 import HighlightedEntryInput from './HighlightedEntryInput';
 import { setTextInput } from '../redux';
+
+
+const styles = StyleSheet.create({
+  entryContainer: {
+    paddingHorizontal: 12,
+    paddingVertical: 6,
+  },
+});
 
 
 class EntryInput extends Component {
@@ -12,17 +20,18 @@ class EntryInput extends Component {
     height: 22,
   }
 
-  onChangeText = (text) => {
-    console.log(text);
+  onChange = (e) => {
+    const { text } = e.nativeEvent;
     const textInput = text.replace(/\n/g, '');
     const { onChangeText } = this.props;
     if (onChangeText) onChangeText(textInput);
-  }
 
-  onContentSizeChange = (e) => {
     const { height } = e.nativeEvent.contentSize;
     this.setState({ height });
   }
+
+  textInput = null;
+  forceSelection = null;
 
   render() {
     let { textInput, result } = this.props; // eslint-disable-line
@@ -30,14 +39,31 @@ class EntryInput extends Component {
 
     textInput = textInput.replace(/\n/g, '');
 
+    // We use blurOnSubmit because it'll stop adding multiple lines
+    // See RCTTextView.m:
+    //
+    // TODO: the purpose of blurOnSubmit on RCTextField is to decide if the
+    // field should lose focus when return is pressed or not. We're cheating a
+    // bit here by using it on RCTextView to decide if return character should
+    // submit the form, or be entered into the field.
+    //
+    // The reason this is cheating is because there's no way to specify that
+    // you want the return key to be swallowed *and* have the field retain
+    // focus (which was what blurOnSubmit was originally for). For the case
+    // where _blurOnSubmit = YES, this is still the correct and expected
+    // behavior though, so we'll leave the don't-blur-or-add-newline problem
+    // to be solved another day.
+
     return (
-      <View>
+      <View style={styles.entryContainer}>
         <TextInput
+          ref={textInput => { this.textInput = textInput; }}
           style={{ width: 300, height }}
           placeholder="Type to begin calculation…"
-          onChangeText={this.onChangeText}
-          onContentSizeChange={this.onContentSizeChange}
+          returnKeyType="done"
+          onChange={this.onChange}
           multiline
+          blurOnSubmit
         >
           <HighlightedEntryInput
             text={textInput}
